1127a84aa933cac8b675aec479e62dbe7f097bd5
adamPetho <petho.adam@hotmail.com>
2026-09-08 20:41:39 +0200

    add JSON event parser

---
+++ Sonrisa-Homework-Webhooks/Parser/EventFactory.cs
@@ -0,0 +1,32 @@
+﻿using Sonrisa_Homework_Webhooks.Models.Events;
+using System.Text.Json;
+
+namespace Sonrisa_Homework_Webhooks.Parser
+{
+    public sealed class EventFactory : IEventFactory
+    {
+        public IEvent Create(EventRequest request)
+        {
+            return request.Type.ToLowerInvariant() switch
+            {
+                "market.movement" => CreateMarketMovement(request.Data),
+
+                _ => throw new NotSupportedException(
+                    $"Event type '{request.Type}' is not supported.")
+            };
+        }
+
+        private static MarketMovementEvent CreateMarketMovement(JsonElement data)
+        {
+            var marketEvent = data.Deserialize<MarketMovementData>() ?? throw new JsonException("Invalid market movement data.");
+
+            return new MarketMovementEvent(
+                Guid.NewGuid(),
+                marketEvent.Symbol,
+                marketEvent.ChangePercentage,
+                marketEvent.OccurredAt);
+        }
+    }
+
+    public sealed record MarketMovementData(string Symbol, decimal ChangePercentage, DateTimeOffset OccurredAt);
+}
---
+++ Sonrisa-Homework-Webhooks/Parser/IEventFactory.cs
@@ -0,0 +1,9 @@
+﻿using Sonrisa_Homework_Webhooks.Models.Events;
+
+namespace Sonrisa_Homework_Webhooks.Parser
+{
+    public interface IEventFactory
+    {
+        IEvent Create(EventRequest request);
+    }
+}
