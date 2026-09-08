0f0594ec8d4239dffa292c9ceffd429421e4216a
adamPetho <petho.adam@hotmail.com>
2026-09-08 19:15:17 +0200

    Wire up services in Program.cs

--- Sonrisa-Homework-Webhooks/Program.cs
+++ Sonrisa-Homework-Webhooks/Program.cs
@@ -1,3 +1,7 @@
+using Sonrisa_Homework_Webhooks.Repositories;
+using Sonrisa_Homework_Webhooks.Services;
+using Sonrisa_Homework_Webhooks.Services.AlertEvaluators;
+using Sonrisa_Homework_Webhooks.Services.NotificationDispatchers;
 
 namespace Sonrisa_Homework_Webhooks
 {
@@ -8,6 +12,18 @@ namespace Sonrisa_Homework_Webhooks
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddSingleton<IAlertRepository, InMemoryAlertRepository>();
+            builder.Services.AddSingleton<IEventRepository, InMemoryEventRepository>();
+
+            builder.Services.AddSingleton<IMessageQueue, MessageQueue>();
+
+            builder.Services.AddSingleton<IAlertEngine, AlertEngine>();
+
+            builder.Services.AddSingleton<IAlertEvaluator, MarketMovementAlertEvaluator>();
+
+            builder.Services.AddSingleton<INotificationDispatcher, NotificationDispatcher>();
+
+            builder.Services.AddHostedService<NotificationWorker>();
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -24,7 +40,6 @@ namespace Sonrisa_Homework_Webhooks
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
