0a396851b2fccd356ee5f1e49928b1aa6b194f67
adamPetho <petho.adam@hotmail.com>
2026-09-08 19:09:21 +0200

    Add MessageQueue to process Notifications async

---
+++ Sonrisa-Homework-Webhooks/Services/IMessageQueue.cs
@@ -0,0 +1,11 @@
+﻿using Sonrisa_Homework_Webhooks.Models;
+
+namespace Sonrisa_Homework_Webhooks.Services
+{
+    public interface IMessageQueue
+    {
+        ValueTask EnqueueAsync(Notification notification, CancellationToken cancellationToken);
+
+        ValueTask<Notification> DequeueAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Sonrisa-Homework-Webhooks/Services/MessageQueue.cs
@@ -0,0 +1,27 @@
+﻿using Sonrisa_Homework_Webhooks.Models;
+using System.Threading.Channels;
+
+namespace Sonrisa_Homework_Webhooks.Services
+{
+
+    public sealed class MessageQueue : IMessageQueue
+    {
+        private readonly Channel<Notification> _channel =
+            Channel.CreateUnbounded<Notification>();
+
+        public ValueTask EnqueueAsync(
+            Notification notification,
+            CancellationToken cancellationToken)
+        {
+            return _channel.Writer.WriteAsync(
+                notification,
+                cancellationToken);
+        }
+
+        public ValueTask<Notification> DequeueAsync(
+            CancellationToken cancellationToken)
+        {
+            return _channel.Reader.ReadAsync(cancellationToken);
+        }
+    }
+}
