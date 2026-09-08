dd127bf3a955d6e54361e241a3a0f67111e756a3
adamPetho <petho.adam@hotmail.com>
2026-09-08 19:04:21 +0200

    add IEvent and MarketMove Event

---
+++ Sonrisa-Homework-Webhooks/Models/Events/IEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Sonrisa_Homework_Webhooks.Models.Events
+{
+    public interface IEvent
+    {
+        Guid Id { get; }
+        string Type { get; }
+        DateTimeOffset OccurredAt { get; }
+    }
+}
---
+++ Sonrisa-Homework-Webhooks/Models/Events/MarketMovementEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Sonrisa_Homework_Webhooks.Models.Events
+{
+    public sealed record MarketMovementEvent(
+        Guid Id,
+        string Symbol,
+        decimal ChangePercentage,
+        DateTimeOffset OccurredAt) : IEvent
+    {
+        public string Type => "market.movement";
+    }
+}
