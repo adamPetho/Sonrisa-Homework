2042ae6b38263f644837018d8723d740e51d360c
adamPetho <petho.adam@hotmail.com>
2026-09-08 19:08:48 +0200

    Create Alert Evaluator to only fire Notifications if it hits the user's rules

---
+++ Sonrisa-Homework-Webhooks/Services/AlertEvaluators/IAlertEvaluator.cs
@@ -0,0 +1,11 @@
+﻿using Sonrisa_Homework_Webhooks.Models.Alerts;
+using Sonrisa_Homework_Webhooks.Models.Events;
+
+namespace Sonrisa_Homework_Webhooks.Services.AlertEvaluators
+{
+    public interface IAlertEvaluator
+    {
+        bool CanEvaluate(IEvent @event);
+        bool Matches(IAlert alert, IEvent @event);
+    }
+}
---
+++ Sonrisa-Homework-Webhooks/Services/AlertEvaluators/MarketMovementAlertEvaluator.cs
@@ -0,0 +1,28 @@
+﻿using Sonrisa_Homework_Webhooks.Models.Alerts;
+using Sonrisa_Homework_Webhooks.Models.Events;
+
+namespace Sonrisa_Homework_Webhooks.Services.AlertEvaluators
+{
+    public sealed class MarketMovementAlertEvaluator : IAlertEvaluator
+    {
+        public bool CanEvaluate(IEvent @event)
+            => @event is MarketMovementEvent;
+
+        public bool Matches(IAlert alert, IEvent @event)
+        {
+            if (@event is not MarketMovementEvent marketEvent)
+                return false;
+
+            if (alert is not MarketMovementAlert marketAlert)
+                return false;
+
+            if (marketAlert.Symbol != marketEvent.Symbol)
+                return false;
+
+            if (Math.Abs(marketEvent.ChangePercentage) >= marketAlert.ThresholdPercentage)
+                return true;
+
+            return false;
+        }
+    }
+}
