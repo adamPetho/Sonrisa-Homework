a61dd1b0d4c7b8dd8b98590447677e3c9d385ff2
adamPetho <petho.adam@hotmail.com>
2026-09-08 19:10:10 +0200

    Create Notification Dispatcher to prepare the ground for future Notification channels like Email, Slack, Discord...

---
+++ Sonrisa-Homework-Webhooks/Services/NotificationDispatchers/INotificationDispatcher.cs
@@ -0,0 +1,11 @@
+﻿using Sonrisa_Homework_Webhooks.Models;
+
+namespace Sonrisa_Homework_Webhooks.Services.NotificationDispatchers
+{
+    public interface INotificationDispatcher
+    {
+        Task DispatchAsync(
+            Notification notification,
+            CancellationToken cancellationToken);
+    }
+}
---
+++ Sonrisa-Homework-Webhooks/Services/NotificationDispatchers/NotificationDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using Sonrisa_Homework_Webhooks.Models;
+
+namespace Sonrisa_Homework_Webhooks.Services.NotificationDispatchers
+{
+    public sealed class NotificationDispatcher : INotificationDispatcher
+    {
+        private readonly IReadOnlyDictionary<string, INotificationChannel> _channels;
+
+        public NotificationDispatcher(
+            IEnumerable<INotificationChannel> channels)
+        {
+            _channels = channels.ToDictionary(
+                x => x.Name,
+                StringComparer.OrdinalIgnoreCase);
+        }
+
+        public async Task DispatchAsync(Notification notification, CancellationToken cancellationToken)
+        {
+            if (!_channels.TryGetValue(notification.Channel, out var channel))
+            {
+                throw new InvalidOperationException(
+                    $"Unknown notification channel: {notification.Channel}");
+            }
+
+            await channel.SendAsync(notification, cancellationToken);
+        }
+    }
+}
