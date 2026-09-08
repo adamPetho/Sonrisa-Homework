a4c0ec1adbd27f7284144d8b16dd7c32193a9c44
adamPetho <petho.adam@hotmail.com>
2026-09-08 20:42:12 +0200

    Create EventController to simulate incoming events

---
+++ Sonrisa-Homework-Webhooks/Controllers/EventsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sonrisa_Homework_Webhooks.Models.Events;
+using Sonrisa_Homework_Webhooks.Parser;
+using Sonrisa_Homework_Webhooks.Services;
+using System.Text.Json;
+
+namespace Sonrisa_Homework_Webhooks.Controllers
+{
+    [ApiController]
+    [Route("api/events")]
+    public sealed class EventsController : ControllerBase
+    {
+        private readonly IEventFactory _eventFactory;
+        private readonly IAlertEngine _alertEngine;
+
+        public EventsController(
+            IEventFactory eventFactory,
+            IAlertEngine alertEngine)
+        {
+            _eventFactory = eventFactory;
+            _alertEngine = alertEngine;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Receive([FromBody] EventRequest request, CancellationToken cancellationToken)
+        {
+            IEvent @event;
+
+            try
+            {
+                @event = _eventFactory.Create(request);
+            }
+            catch (NotSupportedException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (JsonException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            await _alertEngine.ProcessEventAsync(@event, cancellationToken);
+
+            return Accepted();
+        }
+    }
+}
---
+++ Sonrisa-Homework-Webhooks/Models/Events/EventRequest.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.Json;
+
+namespace Sonrisa_Homework_Webhooks.Models.Events
+{
+    public sealed record EventRequest(string Type, JsonElement Data);
+}
