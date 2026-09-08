70c138a5df675558dd9b8000ea810b54928405de
adamPetho <petho.adam@hotmail.com>
2026-09-08 19:04:31 +0200

    add Notification model

---
+++ Sonrisa-Homework-Webhooks/Models/Notification.cs
@@ -0,0 +1,27 @@
+﻿namespace Sonrisa_Homework_Webhooks.Models
+{
+    public sealed class Notification
+    {
+        public Guid Id { get; init; }
+
+        public Guid AlertId { get; init; }
+
+        public Guid EventId { get; init; }
+
+        public string Channel { get; init; } = null!;
+
+        public string Subject { get; init; } = null!;
+
+        public string Message { get; init; } = null!;
+
+        public DateTimeOffset CreatedAt { get; init; }
+
+        public NotificationStatus Status { get; set; }
+    }
+    public enum NotificationStatus
+    {
+        Pending,
+        Sent,
+        Failed
+    }
+}
