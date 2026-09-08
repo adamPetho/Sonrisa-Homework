929314641c41bddee8a63b20b5d0fae957e8f062
adamPetho <petho.adam@hotmail.com>
2026-09-08 19:07:07 +0200

    add InMemoryEvent repo and interface

---
+++ Sonrisa-Homework-Webhooks/Repositories/IEventRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sonrisa_Homework_Webhooks.Models.Events;
+
+namespace Sonrisa_Homework_Webhooks.Repositories
+{
+    public interface IEventRepository
+    {
+        IReadOnlyCollection<IEvent> GetAll();
+        IEvent? GetById(Guid id);
+        void Add(IEvent alert);
+        void Update(IEvent alert);
+        void Delete(Guid id);
+    }
+}
---
+++ Sonrisa-Homework-Webhooks/Repositories/InMemoryEventRepository.cs
@@ -0,0 +1,46 @@
+﻿using Sonrisa_Homework_Webhooks.Models.Events;
+using System.Collections.Concurrent;
+
+namespace Sonrisa_Homework_Webhooks.Repositories
+{
+    public class InMemoryEventRepository : IEventRepository
+    {
+        private readonly ConcurrentDictionary<Guid, IEvent> Events = new();
+        public void Add(IEvent alert)
+        {
+            if (!Events.TryAdd(alert.Id, alert))
+            {
+                throw new InvalidOperationException(
+                    $"An alert with ID '{alert.Id}' already exists.");
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            Events.TryRemove(id, out _);
+        }
+
+        public IReadOnlyCollection<IEvent> GetAll()
+        {
+            return Events.Values.ToArray();
+        }
+
+        public IEvent? GetById(Guid id)
+        {
+            return Events.TryGetValue(id, out var alert)
+                ? alert
+                : null;
+        }
+
+        public void Update(IEvent alert)
+        {
+            if (!Events.TryGetValue(alert.Id, out _))
+            {
+                throw new KeyNotFoundException(
+                    $"Alert with ID '{alert.Id}' was not found.");
+            }
+
+            Events[alert.Id] = alert;
+        }
+    }
+}
