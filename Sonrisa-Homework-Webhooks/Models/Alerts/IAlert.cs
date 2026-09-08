a0b4c00dced17b404500d8b6dd1766eb242d298a
adamPetho <petho.adam@hotmail.com>
2026-09-08 19:04:00 +0200

    Add IAlert and MarketMovement alert

---
+++ Sonrisa-Homework-Webhooks/Models/Alerts/IAlert.cs
@@ -0,0 +1,11 @@
+﻿namespace Sonrisa_Homework_Webhooks.Models.Alerts
+{
+    public interface IAlert
+    {
+        Guid Id { get; }
+        string Name { get; }
+        string EventType { get; }
+        bool Enabled { get; }
+        IReadOnlyCollection<string> Channels { get; }
+    }
+}
---
+++ Sonrisa-Homework-Webhooks/Models/Alerts/MarketMovementAlert.cs
@@ -0,0 +1,11 @@
+﻿namespace Sonrisa_Homework_Webhooks.Models.Alerts
+{
+    public sealed record MarketMovementAlert(
+        Guid Id,
+        string Name,
+        string EventType,
+        bool Enabled,
+        string Symbol,
+        decimal ThresholdPercentage,
+        IReadOnlyCollection<string> Channels) : IAlert;
+}
