193f326e8158041afbeee8455e0f614f1ef60ab4
adamPetho <petho.adam@hotmail.com>
2026-09-08 19:06:57 +0200

    add InMemoryAlert repo and interface

---
+++ Sonrisa-Homework-Webhooks/Repositories/IAlertRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sonrisa_Homework_Webhooks.Models.Alerts;
+
+namespace Sonrisa_Homework_Webhooks.Repositories
+{
+    public interface IAlertRepository
+    {
+        IReadOnlyCollection<IAlert> GetAll();
+        IAlert? GetById(Guid id);
+        void Add(IAlert alert);
+        void Update(IAlert alert);
+        void Delete(Guid id);
+    }
+}
---
+++ Sonrisa-Homework-Webhooks/Repositories/InMemoryAlertRepository.cs
@@ -0,0 +1,46 @@
+﻿using Sonrisa_Homework_Webhooks.Models.Alerts;
+using System.Collections.Concurrent;
+
+namespace Sonrisa_Homework_Webhooks.Repositories
+{
+    public class InMemoryAlertRepository : IAlertRepository
+    {
+        private readonly ConcurrentDictionary<Guid, IAlert> Alerts = new();
+        public void Add(IAlert alert)
+        {
+            if (!Alerts.TryAdd(alert.Id, alert))
+            {
+                throw new InvalidOperationException(
+                    $"An alert with ID '{alert.Id}' already exists.");
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            Alerts.TryRemove(id, out _);
+        }
+
+        public IReadOnlyCollection<IAlert> GetAll()
+        {
+            return Alerts.Values.ToArray();
+        }
+
+        public IAlert? GetById(Guid id)
+        {
+            return Alerts.TryGetValue(id, out var alert)
+                ? alert
+                : null;
+        }
+
+        public void Update(IAlert alert)
+        {
+            if (!Alerts.TryGetValue(alert.Id, out _))
+            {
+                throw new KeyNotFoundException(
+                    $"Alert with ID '{alert.Id}' was not found.");
+            }
+
+            Alerts[alert.Id] = alert;
+        }
+    }
+}
